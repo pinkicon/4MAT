8658df942ff109d8f528ee8357cfe31b34e44d26
pinkicon <pinkicon@nexon.co.kr>
2017-01-02 15:26:01 +0900

    포트변경

--- App/runserver.py
+++ App/runserver.py
@@ -8,7 +8,7 @@ from App import app
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(environ.get('SERVER_PORT', '55555'))
+        PORT = int(environ.get('SERVER_PORT', '7704'))
     except ValueError:
-        PORT = 55555
+        PORT = 7704
     app.run(HOST, PORT)
