0ba11dfb75732b32dbfaf8c3a5513a4d3281bb94
pinkicon <pinkicon@nexon.co.kr>
2017-01-03 16:00:12 +0900

    END

--- App/App/views.py
+++ App/App/views.py
@@ -31,6 +31,11 @@ def executeUpdate(query):
     conn.commit()  
     close_db(conn) 
 
+@app.route('/logout')
+def logout():
+    del session["id"] 
+    return render_template("index.html", title='Home Page',  year=datetime.now().year, id = "" )  
+
 def execute(query):
     return true  
 
@@ -61,15 +66,29 @@ def partA():
             return render_template("index.html", title='Home Page',  year=datetime.now().year, id = "" )  
     else : 
         if request.method == 'POST':   
-            query = """ delete from t_answerchoice """ 
+
+            query = """ delete from t_answerchoice where sessionKey = '""" +  session["id"] + """'""" 
             executeUpdate(query)  
+
             for item in request.form :   
                 if request.form[item] != "" :   
-                    query = """ INSERT INTO t_answerchoice ( ChoiceCaseNo, Point )  VALUES (""" + item  + """,""" +  request.form[item] + """)"""
+                    query = """ INSERT INTO t_answerchoice ( ChoiceCaseNo, Point , SessionKey)  
+                                                    VALUES (""" + item  + """,""" +  request.form[item]  + """,'""" +  session["id"] + """')"""
                     executeUpdate(query)  
             return render_template('graph_chartjs.html', title='GRAPH',  message='THE 4MAT SYSTEM') 
         else : 
-            return render_template('partA.html',  examples = executeQuery('''SELECT exampleNo, exampleTitle  From t_example where exampleType='A' order by exampleNo asc limit 10'''), choices  = executeQuery('''SELECT A.exampleNo, A. exampleTItle  , B.Title ChoiceCase , B.ChoiceCaseNo, B.FiqureType  FROM t_example A, t_choicecase B  WHERE A.exampleNo = B.exampleNo order by A.exampleNo asc '''))
+            return render_template('partA.html',  
+                                   examples = executeQuery('''
+                                                                            SELECT exampleNo, exampleTitle  
+                                                                            From t_example
+                                                                            where exampleType='A' 
+                                                                            order by exampleNo asc limit 10'''), 
+                                   choices  = executeQuery('''
+                                                                            SELECT A.exampleNo, A. exampleTItle  , B.Title ChoiceCase , B.ChoiceCaseNo, B.FiqureType  
+                                                                            FROM t_example A, t_choicecase B  
+                                                                            WHERE A.exampleNo = B.exampleNo 
+                                                                            order by A.exampleNo asc ''')
+                                   )
 
 
 @app.route('/partB')
@@ -81,22 +100,33 @@ def graph():
     if not  session["id"] : 
             return render_template("index.html", title='Home Page',  year=datetime.now().year, id = "" )  
     else :  
-        Items = executeQuery('''  SELECT SUM(A.Point) POINT  , C.FiqureType FigureType  FROM t_answerchoice A, t_choiceCase C  WHERE A.ChoiceCaseNo = C.ChoiceCaseNo GROUP BY C.FiqureType''') 
-   
-        if Items.count.__eq__(0) : 
-            circle = 0
-            square = 0
-            star   = 0 
-            triangle = 0 
-        else : 
-            for item in Items:
-                if item[1] == 'CIRCLE': 
-                    circle = str(item[0])
-                elif item[1] == 'SQUARE':
-                    square = str(item[0])
-                elif item[1] == 'STAR':
-                    star = str(item[0])
-                elif item[1] == 'TRIANGLE':
-                    triangle = str(item[0])
+        Items = executeQuery("""
+                                SELECT SUM(A.Point) POINT  , C.FiqureType  FigureType , A.SessionKey 
+                                FROM t_answerchoice A, t_choiceCase C  
+                                WHERE A.ChoiceCaseNo = C.ChoiceCaseNo 
+                                GROUP BY C.FiqureType HAVING SessionKey = '""" +  session["id"] + """'""" )
 
-        return render_template('graph_chartjs.html', title='GRAPH', message='THE 4MAT SYSTEM',   T_circle = circle , T_square = square, T_star = star , T_triangle = triangle   )
+         #Items = executeQuery('''
+         #                       SELECT SUM(A.Point) POINT  , C.FiqureType  FigureType  
+         #                       FROM t_answerchoice A, t_choiceCase C  
+         #                       WHERE A.ChoiceCaseNo = C.ChoiceCaseNo 
+         #                       GROUP BY C.FiqureType  
+         #                     ''')
+  
+    #if Items.count.__eq__(0) : 
+    #        circle = 0
+    #        square = 0
+    #        star   = 0 
+    #        triangle = 0 
+    #else : 
+    for item in Items:
+        if item[1] == 'CIRCLE': 
+            circle = str(item[0])
+        elif item[1] == 'SQUARE':
+            square = str(item[0])
+        elif item[1] == 'STAR':
+            star = str(item[0])
+        elif item[1] == 'TRIANGLE':
+            triangle = str(item[0])
+
+    return render_template('graph_chartjs.html', title='GRAPH', message='THE 4MAT SYSTEM',   T_circle = circle , T_square = square, T_star = star , T_triangle = triangle   )
