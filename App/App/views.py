1888d81877a86769ec14f057dc2e7e1df54c2ffe
pinkicon <pinkicon@nexon.co.kr>
2017-01-03 11:45:55 +0900

    체크

--- App/App/views.py
+++ App/App/views.py
@@ -11,6 +11,9 @@ import pymysql
 def connect_db():
     return pymysql.connect(host='localhost' , user='root', password='root', db='world', charset='utf8') 
 
+def close_db(conn):
+    conn.close() 
+
 def executeQuery(query) : 
     conn = connect_db()
     curs = conn.cursor()
@@ -18,7 +21,7 @@ def executeQuery(query) :
     rv = [] 
     for row in curs._rows : 
         rv.append(row)
-    conn.close()
+    close_db(conn) 
     return rv  
 
 def executeUpdate(query): 
@@ -26,7 +29,7 @@ def executeUpdate(query):
     curs = conn.cursor() 
     curs.execute(query)
     conn.commit()  
-    conn.close()
+    close_db(conn) 
 
 def execute(query):
     return true  
@@ -42,94 +45,58 @@ def teardown_request(exception):
 
 @app.before_request
 def before_request():
-     g.db = connect_db() 
+     g.db = connect_db()  
 
-
-@app.route('/')
-@app.route('/home')
-def home():
-
-    pageUrl = 'index.html' 
-    """Renders the home page."""
-    return render_template(
-        pageUrl,
-        title='Home Page',
-        year=datetime.now().year,
-    ) 
-
-@app.route('/contact')
-def contact():
-    """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
-
-@app.route('/about')
-def about():
-    """Renders the about page."""
-    return render_template(
-        'about.html',
-        title='About',
-        year=datetime.now().year,
-        message='Your application description page.'
-    )
+@app.route('/' ,methods=['GET','POST']) 
+def home():   
+        if request.method == 'POST':
+            session["id"] = request.form["id"]  
+            return render_template("index.html", title='Home Page',  year=datetime.now().year, id = session["id"] )  
+        else : 
+            return render_template("index.html", title='Home Page',  year=datetime.now().year, id = "" )  
 
 @app.route('/partA', methods=['GET','POST']) 
 def partA(): 
-    if request.method == 'POST':  
-
-        query = """ delete from t_answerchoice """ 
-        executeUpdate(query) 
-
-        for item in request.form :   
-            if request.form[item] != "" :   
-                query = """ INSERT INTO t_answerchoice ( ChoiceCaseNo, Point )  VALUES (""" + item  + """,""" +  request.form[item] + """)"""
-                executeUpdate(query)  
-        return render_template('graph_chartjs.html',
-                        title='GRAPH',
-                        message='THE 4MAT SYSTEM')
-
+    if not  session["id"] : 
+            return render_template("index.html", title='Home Page',  year=datetime.now().year, id = "" )  
     else : 
-        return render_template('partA.html', 
-                            examples = executeQuery('''SELECT exampleNo, exampleTitle  From t_example where exampleType='A' order by exampleNo asc limit 10'''),
-                            choices  = executeQuery('''SELECT A.exampleNo, A. exampleTItle  , B.Title ChoiceCase , B.ChoiceCaseNo, B.FiqureType  FROM t_example A, t_choicecase B 
-                                                        WHERE A.exampleNo = B.exampleNo order by A.exampleNo asc '''))
+        if request.method == 'POST':   
+            query = """ delete from t_answerchoice """ 
+            executeUpdate(query)  
+            for item in request.form :   
+                if request.form[item] != "" :   
+                    query = """ INSERT INTO t_answerchoice ( ChoiceCaseNo, Point )  VALUES (""" + item  + """,""" +  request.form[item] + """)"""
+                    executeUpdate(query)  
+            return render_template('graph_chartjs.html', title='GRAPH',  message='THE 4MAT SYSTEM') 
+        else : 
+            return render_template('partA.html',  examples = executeQuery('''SELECT exampleNo, exampleTitle  From t_example where exampleType='A' order by exampleNo asc limit 10'''), choices  = executeQuery('''SELECT A.exampleNo, A. exampleTItle  , B.Title ChoiceCase , B.ChoiceCaseNo, B.FiqureType  FROM t_example A, t_choicecase B  WHERE A.exampleNo = B.exampleNo order by A.exampleNo asc '''))
 
 
 @app.route('/partB')
 def partB(): 
-    return render_template('partB.html',
-                           title='PART B',
-                           message='각 문항마다 가장 당신을 잘 표현하는 항목에 표시하시오') 
+    return render_template('partB.html', title='PART B',  message='각 문항마다 가장 당신을 잘 표현하는 항목에 표시하시오') 
 
 @app.route('/graph')
-def graph(): 
+def graph():  
+    if not  session["id"] : 
+            return render_template("index.html", title='Home Page',  year=datetime.now().year, id = "" )  
+    else :  
+        Items = executeQuery('''  SELECT SUM(A.Point) POINT  , C.FiqureType FigureType  FROM t_answerchoice A, t_choiceCase C  WHERE A.ChoiceCaseNo = C.ChoiceCaseNo GROUP BY C.FiqureType''') 
+   
+        if Items.count.__eq__(0) : 
+            circle = 0
+            square = 0
+            star   = 0 
+            triangle = 0 
+        else : 
+            for item in Items:
+                if item[1] == 'CIRCLE': 
+                    circle = str(item[0])
+                elif item[1] == 'SQUARE':
+                    square = str(item[0])
+                elif item[1] == 'STAR':
+                    star = str(item[0])
+                elif item[1] == 'TRIANGLE':
+                    triangle = str(item[0])
 
-    Items = executeQuery('''
-                                                SELECT SUM(A.Point) POINT  , C.FiqureType FigureType  FROM t_answerchoice A, t_choiceCase C 
-                                                WHERE A.ChoiceCaseNo = C.ChoiceCaseNo 
-                                                GROUP BY C.FiqureType''')
-
-    #circle = ""
-    #triangle = ""
-    #star = ""
-    #square = ""
-
-    for item in Items:
-        if item[1] == 'CIRCLE': 
-            circle = str(item[0])
-        elif item[1] == 'SQUARE':
-            square = str(item[0])
-        elif item[1] == 'STAR':
-            star = str(item[0])
-        elif item[1] == 'TRIANGLE':
-            triangle = str(item[0])
-
-    return render_template('graph_chartjs.html',
-                           title='GRAPH',
-                           message='THE 4MAT SYSTEM', 
-                           T_circle = circle , T_square = square, T_star = star , T_triangle = triangle
-                           )
+        return render_template('graph_chartjs.html', title='GRAPH', message='THE 4MAT SYSTEM',   T_circle = circle , T_square = square, T_star = star , T_triangle = triangle   )
--- App/runserver.py
+++ App/runserver.py
@@ -6,10 +6,15 @@ from os import environ
 from App import app
 
 if __name__ == '__main__':
+
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(environ.get('SERVER_PORT', '7704'))
     except ValueError:
         PORT = 7704
+
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'memcache'  
+    app.debug = True
     app.run(HOST, PORT)
     
